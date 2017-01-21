fb616f716cc36bb7d83e010d60f9a7e6abb5f77c
Liviu Ionescu <ilg@livius.net>
2017-01-22 00:56:44 +0200

    Update travis.osx.sh

--- scripts/travis.osx.sh
+++ scripts/travis.osx.sh
@@ -33,11 +33,11 @@ $HOME/Eclipse.app/Contents/MacOS/eclipse --launcher.suppressErrors -nosplash \
 -application org.eclipse.cdt.managedbuilder.core.headlessbuild \
 -data $HOME/workspace \
 -import $HOME/build/micro-os-plus/eclipse-test-projects/synthetic-posix-tests-micro-os-plus \
--cleanBuild test-cmsis-rtos-valid-clang-debug \
--cleanBuild test-cmsis-rtos-valid-clang-release \
--cleanBuild test-mutex-stress-clang-debug \
--cleanBuild test-mutex-stress-clang-release \
--cleanBuild test-rtos-clang-debug \
--cleanBuild test-rtos-clang-release \
+-cleanBuild synthetic-posix-tests-micro-os-plus/test-cmsis-rtos-valid-clang-debug \
+-cleanBuild synthetic-posix-tests-micro-os-plus/test-cmsis-rtos-valid-clang-release \
+-cleanBuild synthetic-posix-tests-micro-os-plus/test-mutex-stress-clang-debug \
+-cleanBuild synthetic-posix-tests-micro-os-plus/test-mutex-stress-clang-release \
+-cleanBuild synthetic-posix-tests-micro-os-plus/test-rtos-clang-debug \
+-cleanBuild synthetic-posix-tests-micro-os-plus/test-rtos-clang-release \
 
 
