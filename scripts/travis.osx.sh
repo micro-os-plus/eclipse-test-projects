27a0d33f0c99e7514ba2be4f0b18d38e08691e8b
Liviu Ionescu <ilg@livius.net>
2017-01-22 00:02:53 +0200

    Update travis.osx.sh

--- scripts/travis.osx.sh
+++ scripts/travis.osx.sh
@@ -20,7 +20,7 @@ java -version
 cd $HOME
 
 eclipse_archive_name=eclipse-cpp-mars-2-macosx-cocoa-x86_64.tar.gz
-curl -L "http://artfiles.org/eclipse.org//technology/epp/downloads/release/mars/2/${eclipse_archive_name}
+curl -L "http://artfiles.org/eclipse.org//technology/epp/downloads/release/mars/2/${eclipse_archive_name}"
 -o $HOME/Downloads/${eclipse_archive_name}
 
 tar xf $HOME/Downloads/${eclipse_archive_name}
